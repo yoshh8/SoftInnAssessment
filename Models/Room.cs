c3fbe1ea3460117591b8cfbe3009a6094fcb69d7
yoshh8 <rahmanasaherry@gmail.com>
2025-10-01 09:56:28 +0800

    Naming Violations

--- Models/Room.cs
+++ Models/Room.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
-        public bool isAvailable { get; set; }
+        public bool IsAvailable { get; set; }
 
     }
 }
